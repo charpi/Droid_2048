ade631d3f56a18487a83bbd806c093c69d817dd8
Nicolas Charpentier <nc@charpi.net>
2014-07-02 09:14:28 +0100

    Initial commit with the basic game engine

---
+++ Droid_2048/test_lib/Test.fs
@@ -0,0 +1,14 @@
+﻿namespace test_lib
+open System
+open NUnit.Framework
+open Droid_2048
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member x.TestCase() =
+        let expected = {GameGrid.score = 0; GameGrid.grid =  [[0; 0; 0; 0]; [4; 0; 0; 0]; [0; 0; 0; 0]; [0; 0; 0; 0]]} 
+        let start = {GameGrid.score = 0; GameGrid.grid =  [[0; 0; 0; 0]; [2; 2; 0; 0]; [0; 0; 0; 0]; [0; 0; 0; 0]]} 
+        Assert.Equals(expected, start)
+        
